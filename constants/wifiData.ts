856e5b626ac8b76b4c0134888331718a91285a09
alishba29 <khaweralishba@gmail.com>
2025-03-23 02:10:41 +0500

    alishbas home locations

--- constants/wifiData.ts
+++ constants/wifiData.ts
@@ -103,6 +103,231 @@ const wifiData = [
         "signalStrength": "-91.0"
       }
     ]
+  },
+  {
+    "location": "Alishba's room",
+    "signals": [
+      {
+        "ssid": "🇱🇾 ",
+        "mac": "74:da:88:07:05:ee",
+        "signalStrength": "-67.0"
+      },
+      {
+        "ssid": "CONNECT 15",
+        "mac": "b4:43:26:77:94:1c",
+        "signalStrength": "-69.0"
+      },
+      {
+        "ssid": "MA",
+        "mac": "ec:08:6b:f6:f5:fc",
+        "signalStrength": "-87.0"
+      },
+      {
+        "ssid": "FTTH-1081-2",
+        "mac": "30:d1:7e:2c:34:d4",
+        "signalStrength": "-32.0"
+      },
+      {
+        "ssid": "Ahsan-5G",
+        "mac": "f4:e4:51:02:6c:e0",
+        "signalStrength": "-73.0"
+      },
+      {
+        "ssid": "Ahsan",
+        "mac": "f4:e4:51:02:6c:dc",
+        "signalStrength": "-85.0"
+      },
+      {
+        "ssid": "FTTH-Salman",
+        "mac": "80:d4:a5:5e:4c:34",
+        "signalStrength": "-82.0"
+      },
+      {
+        "ssid": "FTTH-1080",
+        "mac": "34:6a:c2:05:8a:80",
+        "signalStrength": "-80.0"
+      },
+      {
+        "ssid": "Abdul Aziz-5G",
+        "mac": "14:8c:4a:fc:17:08",
+        "signalStrength": "-73.0"
+      },
+      {
+        "ssid": "FAZAL",
+        "mac": "e0:e0:c2:d0:f9:08",
+        "signalStrength": "-87.0"
+      },
+      {
+        "ssid": "FTTH-1085",
+        "mac": "58:25:75:39:a7:20",
+        "signalStrength": "-83.0"
+      },
+      {
+        "ssid": "TP-LINK_4A50",
+        "mac": "d4:6e:0e:77:4a:50",
+        "signalStrength": "-78.0"
+      },
+      {
+        "ssid": "WirelessNet",
+        "mac": "c0:bf:c0:15:82:30",
+        "signalStrength": "-82.0"
+      },
+      {
+        "ssid": "FTTH-1084",
+        "mac": "d4:6b:a6:37:58:64",
+        "signalStrength": "-70.0"
+      },
+      {
+        "ssid": "TENDA(5G)",
+        "mac": "c8:3a:35:48:8e:50",
+        "signalStrength": "-68.0"
+      },
+      {
+        "ssid": "FTTH-KHAN",
+        "mac": "a4:c6:4f:62:8b:98",
+        "signalStrength": "-87.0"
+      }
+    ]
+  },
+  {
+    "location": "Alishba Room2",
+    "signals": [
+      {
+        "ssid": "🇱🇾 ",
+        "mac": "74:da:88:07:05:ee",
+        "signalStrength": "-72.0"
+      },
+      {
+        "ssid": "CONNECT 15",
+        "mac": "b4:43:26:77:94:1c",
+        "signalStrength": "-78.0"
+      },
+      {
+        "ssid": "MA",
+        "mac": "ec:08:6b:f6:f5:fc",
+        "signalStrength": "-87.0"
+      },
+      {
+        "ssid": "FTTH-1081-2",
+        "mac": "30:d1:7e:2c:34:d4",
+        "signalStrength": "-39.0"
+      },
+      {
+        "ssid": "Ahsan-5G",
+        "mac": "f4:e4:51:02:6c:e0",
+        "signalStrength": "-73.0"
+      },
+      {
+        "ssid": "Ahsan",
+        "mac": "f4:e4:51:02:6c:dc",
+        "signalStrength": "-85.0"
+      },
+      {
+        "ssid": "FTTH-1080",
+        "mac": "34:6a:c2:05:8a:80",
+        "signalStrength": "-80.0"
+      },
+      {
+        "ssid": "Abdul Aziz-5G",
+        "mac": "14:8c:4a:fc:17:08",
+        "signalStrength": "-73.0"
+      },
+      {
+        "ssid": "FTTH-1085",
+        "mac": "58:25:75:39:a7:20",
+        "signalStrength": "-83.0"
+      },
+      {
+        "ssid": "TP-LINK_4A50",
+        "mac": "d4:6e:0e:77:4a:50",
+        "signalStrength": "-82.0"
+      },
+      {
+        "ssid": "WirelessNet",
+        "mac": "c0:bf:c0:15:82:30",
+        "signalStrength": "-75.0"
+      },
+      {
+        "ssid": "FTTH-1084",
+        "mac": "d4:6b:a6:37:58:64",
+        "signalStrength": "-62.0"
+      },
+      {
+        "ssid": "TENDA(5G)",
+        "mac": "c8:3a:35:48:8e:50",
+        "signalStrength": "-69.0"
+      }
+    ]
+  },
+  {
+    "location": "Alishba's lounge ",
+    "signals": [
+      {
+        "ssid": "🇱🇾 ",
+        "mac": "74:da:88:07:05:ee",
+        "signalStrength": "-67.5"
+      },
+      {
+        "ssid": "CONNECT 15",
+        "mac": "b4:43:26:77:94:1c",
+        "signalStrength": "-78.0"
+      },
+      {
+        "ssid": "MA",
+        "mac": "ec:08:6b:f6:f5:fc",
+        "signalStrength": "-87.0"
+      },
+      {
+        "ssid": "FTTH-1081-2",
+        "mac": "30:d1:7e:2c:34:d4",
+        "signalStrength": "-32.5"
+      },
+      {
+        "ssid": "Ahsan-5G",
+        "mac": "f4:e4:51:02:6c:e0",
+        "signalStrength": "-73.0"
+      },
+      {
+        "ssid": "Ahsan",
+        "mac": "f4:e4:51:02:6c:dc",
+        "signalStrength": "-85.0"
+      },
+      {
+        "ssid": "FTTH-1080",
+        "mac": "34:6a:c2:05:8a:80",
+        "signalStrength": "-80.0"
+      },
+      {
+        "ssid": "Abdul Aziz-5G",
+        "mac": "14:8c:4a:fc:17:08",
+        "signalStrength": "-73.0"
+      },
+      {
+        "ssid": "FTTH-1085",
+        "mac": "58:25:75:39:a7:20",
+        "signalStrength": "-83.0"
+      },
+      {
+        "ssid": "TP-LINK_4A50",
+        "mac": "d4:6e:0e:77:4a:50",
+        "signalStrength": "-82.0"
+      },
+      {
+        "ssid": "WirelessNet",
+        "mac": "c0:bf:c0:15:82:30",
+        "signalStrength": "-75.0"
+      },
+      {
+        "ssid": "FTTH-1084",
+        "mac": "d4:6b:a6:37:58:64",
+        "signalStrength": "-60.5"
+      },
+      {
+        "ssid": "TENDA(5G)",
+        "mac": "c8:3a:35:48:8e:50",
+        "signalStrength": "-62.5"
+      }
+    ]
   }
 ];
 
