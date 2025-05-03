8cb8f326816b227f92fdeded5b0051b045c755cb
alishba29 <khaweralishba@gmail.com>
2025-05-03 12:27:28 +0500

    wifi data

--- constants/wifiData.ts
+++ constants/wifiData.ts
@@ -48,19 +48,84 @@ const wifiData = [
     "location": "CSIT Room 2",
     "signals": [
       {
+        "ssid": "AndroidAP",
+        "mac": "c8:d7:b0:dd:3d:f2",
+        "signalStrength": "-73.0"
+      },
+      {
+        "ssid": "barbie marium",
+        "mac": "5e:ec:64:af:28:52",
+        "signalStrength": "-80.0"
+      },
+      {
+        "ssid": "NEDUET-WiFi Network",
+        "mac": "14:d6:4d:a6:f7:33",
+        "signalStrength": "-79.0"
+      },
+      {
         "ssid": "eduroam",
-        "mac": "04:b0:e7:c5:f6:00",
-        "signalStrength": "-62.5"
+        "mac": "04:b0:e7:c6:00:30",
+        "signalStrength": "-90.0"
+      },
+      {
+        "ssid": "eduroam",
+        "mac": "04:b0:e7:c5:f6:10",
+        "signalStrength": "-71.0"
+      },
+      {
+        "ssid": "Redmi A2+",
+        "mac": "ca:93:8e:94:60:17",
+        "signalStrength": "-59.0"
+      },
+      {
+        "ssid": "HUAWEI Mate 10 lite",
+        "mac": "38:37:8b:4e:58:4a",
+        "signalStrength": "-74.0"
+      },
+      {
+        "ssid": "DIRECT-BF-HP ",
+        "mac": "32:24:a9:f6:4b:bf",
+        "signalStrength": "-68.0"
+      },
+      {
+        "ssid": "TECNO SPARK 20",
+        "mac": "d6:80:a7:69:3b:cb",
+        "signalStrength": "-69.0"
+      },
+      {
+        "ssid": "shtpppp",
+        "mac": "32:84:54:a2:e8:0f",
+        "signalStrength": "-77.0"
+      },
+      {
+        "ssid": "eduroam",
+        "mac": "04:b0:e7:c6:25:a0",
+        "signalStrength": "-58.0"
       },
       {
         "ssid": "eduroam",
         "mac": "04:b0:e7:c6:25:b0",
-        "signalStrength": "-64.5"
+        "signalStrength": "-66.0"
+      },
+      {
+        "ssid": "realme C35",
+        "mac": "6e:33:39:0d:d8:3b",
+        "signalStrength": "-75.0"
+      },
+      {
+        "ssid": "Galaxy A3213D9",
+        "mac": "d2:10:d4:99:f5:3f",
+        "signalStrength": "-71.0"
+      },
+      {
+        "ssid": "eduroam",
+        "mac": "04:b0:e7:c6:49:70",
+        "signalStrength": "-96.0"
       },
       {
         "ssid": "eduroam",
         "mac": "04:b0:e7:c5:c8:30",
-        "signalStrength": "-85.5"
+        "signalStrength": "-81.0"
       }
     ]
   },
@@ -114,7 +179,7 @@ const wifiData = [
       },
       {
         "ssid": "eduroam",
-        "mac": "04:b0:e7:c5:f6:10",
+        "mac": "04:b0:e7:c5:f6:10", 
         "signalStrength": "-57.0"
       },
       {
@@ -334,63 +399,83 @@ const wifiData = [
     "signals": [
       {
         "ssid": "eduroam",
-        "mac": "04:b0:e7:c6:4b:50",
-        "signalStrength": "-96.0"
+        "mac": "04:b0:e7:c6:00:30",
+        "signalStrength": "-79.0"
       },
       {
         "ssid": "eduroam",
-        "mac": "04:b0:e7:c6:00:30",
-        "signalStrength": "-70.0"
+        "mac": "04:b0:e7:c6:3d:d0",
+        "signalStrength": "-92.0"
       },
       {
         "ssid": "eduroam",
         "mac": "04:b0:e7:c6:00:20",
-        "signalStrength": "-63.0"
+        "signalStrength": "-77.0"
       },
       {
         "ssid": "eduroam",
         "mac": "04:b0:e7:c5:f6:10",
-        "signalStrength": "-70.0"
+        "signalStrength": "-68.0"
+      },
+      {
+        "ssid": "OPPO A95",
+        "mac": "fe:23:bf:08:9b:d4",
+        "signalStrength": "-87.0"
+      },
+      {
+        "ssid": "eduroam",
+        "mac": "04:b0:e7:c5:f6:00",
+        "signalStrength": "-81.0"
       },
       {
         "ssid": "eduroam",
         "mac": "04:b0:e7:c6:29:90",
-        "signalStrength": "-55.0"
+        "signalStrength": "-70.0"
       },
       {
         "ssid": "eduroam",
         "mac": "04:b0:e7:c6:29:80",
-        "signalStrength": "-79.0"
+        "signalStrength": "-71.0"
       },
       {
         "ssid": "eduroam",
         "mac": "04:b0:e7:c6:25:b0",
-        "signalStrength": "-50.0"
-      },
-      {
-        "ssid": "eduroam",
-        "mac": "04:b0:e7:c6:01:c0",
-        "signalStrength": "-79.0"
-      },
-      {
-        "ssid": "eduroam",
-        "mac": "04:b0:e7:c6:01:d0",
-        "signalStrength": "-94.0"
-      },
-      {
-        "ssid": "eduroam",
-        "mac": "84:a9:c4:8c:c8:70",
-        "signalStrength": "-85.0"
+        "signalStrength": "-61.0"
       },
       {
         "ssid": "eduroam",
         "mac": "04:b0:e7:c6:3e:d0",
-        "signalStrength": "-83.0"
+        "signalStrength": "-86.0"
+      },
+      {
+        "ssid": "Redmi 13",
+        "mac": "ba:93:e2:35:18:25",
+        "signalStrength": "-80.0"
+      },
+      {
+        "ssid": "eduroam",
+        "mac": "04:b0:e7:c6:4b:80",
+        "signalStrength": "-78.0"
       },
       {
         "ssid": "eduroam",
         "mac": "04:b0:e7:c5:c8:30",
-        "signalStrength": "-93.0"
+        "signalStrength": "-72.0"
+      },
+      {
+        "ssid": "eduroam",
+        "mac": "04:b0:e7:c6:4b:90",
+        "signalStrength": "-76.0"
+      },
+      {
+        "ssid": "TECNO POVA 2",
+        "mac": "02:a8:26:92:1a:7d",
+        "signalStrength": "-68.0"
+      },
+      {
+        "ssid": "sohaa",
+        "mac": "0e:b8:c5:dd:c6:7c",
+        "signalStrength": "-69.0"
       }
     ]
   },
